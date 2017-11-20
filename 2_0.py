ac92dad36ed33973c4befca37640503fe7f139b6
fcomu <33601408+fcomu@users.noreply.github.com>
2017-11-19 22:44:25 -0300

    Update 2_0.py

--- 2_0.py
+++ 2_0.py
@@ -1,4 +1,4 @@
-from math import *
+import math
 from random import randint
 
 lista = []
@@ -9,23 +9,23 @@ camino = []
 recorrido = []
 NC = 0
 
-def Buscar(lista2, x):
+def Buscar(lista2, x): 
     i = 0
     while (1):
         if lista2[i] == x:
             lista2.pop(i)
         i = i + 1
 
-def Lectura():
+def Lectura(): #maneja la lectura del archivo
     i=0
     archivo = open("archivo.txt", "r")
     lineasarch = (len(open("archivo.txt").readlines())-2)
     for linea in archivo.readlines():
         if i > 5 and i <= lineasarch:
             cadena = linea.split()
-            ejex = float(cadena[1])
-            ejey = float(cadena[2])
-            dis = sqrt((ejex**2)+(ejey**2))
+            ejex = float(cadena[1]) #obtenemos eje x
+            ejey = float(cadena[2]) #obtenemos eje y
+            dis = math.sqrt((ejex**2)+(ejey**2)) #se calcula un tipo de hipotenuza para el nodo
             lista.append(dis)
         i = i + 1
 
@@ -39,7 +39,7 @@ def llenarabeja():
 def randomabeja():
     i = 0
     while i < 5:
-        a = randint(1, 30)
+        a = randint(1, 30) #le damos valores random a las abejas entre 1 y 30
         print a
         camino.append(a)        
         i = i + 1
@@ -58,22 +58,23 @@ def mover():
     i = 0
     j = 0
     k = 0
-    dist = 0.0
+    dist = 0.0 #valor para tener valor de las distancias
     aux = 0.0
-    while lista[i] <= len(lista):
-        while lista2[j] <= len(lista2):
-            dist = abs(lista[i] - lista2[j])
-            if aux == 0:
+    aux_lista = lista 
+    global NC 
+    while i in range(0, len(lista)): #manejo de fila
+        while j in range(0, len(lista2)):  #manejo de columna
+            dist = math.fabs(float(lista[i]) - float(lista2[j])) #calcula distancias
+            if aux == 0 or aux < dist: #compruba nuevo aux de distancia
                 aux = dist
-            
-            if aux > dist:
+            if aux > dist: #si auxilia es mayo se le pasa el menor
                 aux = dist
-                recorrido.append(k)
+                recorrido.append(k) #al recorrido le dara el nodo por los que pasa
                 NC = NC + 1
                 lista2.pop(j)
-                break;
             j = j + 1
             k = k + 1
+        aux_lista.pop(i)
         i = i + 1
              
 
@@ -99,21 +100,28 @@ NC = numeros de pasos constructivos en forwars pass
 10) mostrar mejor solucion
 """
 def BCO():
-    randomabeja()  
+    randomabeja() #inicializa abeja 
     k=0
     iniciarnodo()
     while True:
-        k = k + 1
+        k = k + 1 
         mover()
-        if k <= NC:
+        if k <= NC: #verifica condicion de verdad para BCO
             break
         return BCO
 
 
-
-        
-Lectura()
-lista2 = lista
-llenarabeja()
-BCO()
-print recorrido[1]
+def sumar():
+    i = 0
+    valor = 0 
+    while i in range(0, len(recorrido)): #ciclo para hacer el recorrido
+        valor = valor + recorrido[i] #es el resultado 
+        i = i + 1 #maneja el recorrido
+    print valor #imprime el valor despues del los saltos por los nodo
+    
+Lectura() #lee la informacion de los archivos de texto
+lista2 = lista #se igualan las listas
+lista2.pop(0) #saca primer elemento de la seguna lista
+llenarabeja()   #llena las abjeas con numero random
+BCO() #algoritmo abeja
+sumar() #suma todo los nodos
